dd2e354787bfa4bb55453cca82a2bfbd5de18e38
Thomas Gollenia <thomas@gollenia.at>
2025-11-17 15:06:35 +0100

    reorganized js

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+// webpack.config.js
+const defaultConfig = require('@wordpress/scripts/config/webpack.config');
+const path = require('path');
+
+module.exports = {
+	// Basis-Konfiguration von wp-scripts übernehmen
+	...defaultConfig,
+
+	// Eigene Entry-Points definieren
+	entry: {
+		admin: path.resolve(__dirname, 'src/admin/index.js'),
+		frontend: path.resolve(__dirname, 'src/frontend/index.js'),
+		editor: path.resolve(__dirname, 'src/editor/index.js'),
+	},
+
+	output: {
+		...defaultConfig.output,
+		// Name anhand des Entry-Keys
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'build'),
+	},
+};
