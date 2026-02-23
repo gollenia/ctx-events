e212d524e90984000bc68392c601bd74039f5212
Thomas Gollenia <thomas@gollenia.at>
2026-02-23 07:23:22 +0100

    synced scopes with frontend

--- includes/Event/Domain/Enums/TimeScope.php
+++ includes/Event/Domain/Enums/TimeScope.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 namespace Contexis\Events\Event\Domain\Enums;
 
+use Spatie\TypeScriptTransformer\Attributes\TypeScript;
+
+#[TypeScript]
 enum TimeScope: string
 {
     case ALL = 'all';
@@ -10,14 +13,13 @@ enum TimeScope: string
     case PAST = 'past';
     case TODAY = 'today';
     case TOMORROW = 'tomorrow';
-    case WEEK = 'week';
+    case ONE_WEEK = 'one-week';
     case THIS_WEEK = 'this-week';
     case THIS_MONTH = 'this-month';
     case NEXT_MONTH = 'next-month';
     case ONE_MONTH = '1-months';
     case TWO_MONTHS = '2-months';
     case THREE_MONTHS = '3-months';
-    case SIX_MONTHS = '6-months';
-    case TWELVE_MONTHS = '12-months';
-    case YEAR = 'year';
+    case THIS_YEAR = 'this-year';
+	case ONE_YEAR = '1-year';
 }
