98803cf9013d0adf84ba9cf678bfb36938c0a088
Thomas Gollenia <thomas@gollenia.at>
2026-03-13 08:18:19 +0100

    new dto for booking actions

---
+++ includes/Booking/Application/DTOs/BookingActionRequest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Contexis\Events\Booking\Application\DTOs;
+
+final readonly class BookingActionRequest
+{
+    public function __construct(
+        public string $reference,
+        public bool $sendMail = true,
+    ) {
+    }
+}
