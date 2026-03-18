e125e7828e7fed2e9fa716cc5b5482fa0a9647be
Thomas Gollenia <thomas@gollenia.at>
2026-03-18 06:50:32 +0100

    old booking app removed

--- src/frontend/booking_old/modules/priceUtils.js
+++
@@ -1,8 +0,0 @@
-const formatCurrency = (price, locale, currency) => {
-	return new Intl.NumberFormat(locale, {
-		style: 'currency',
-		currency,
-	}).format(price);
-};
-
-export { formatCurrency };
