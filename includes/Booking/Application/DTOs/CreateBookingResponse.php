f1816923ced3fabeae23a78e7b002fe7c9fd8782
Thomas Gollenia <thomas@gollenia.at>
2026-03-24 14:42:12 +0100

    added Email Result to BookingResponse

--- includes/Booking/Application/DTOs/CreateBookingResponse.php
+++ includes/Booking/Application/DTOs/CreateBookingResponse.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Contexis\Events\Booking\Application\DTOs;
 
+use Contexis\Events\Communication\Application\DTOs\BookingEmailResult;
 use Contexis\Events\Booking\Domain\ValueObjects\BookingReference;
 use Contexis\Events\Payment\Domain\Transaction;
 
@@ -11,12 +12,13 @@ final readonly class CreateBookingResponse
 {
 	public function __construct(
 		public BookingReference $reference,
-		public Transaction $transaction
+		public ?Transaction $transaction,
+        public BookingEmailResult $emailResult,
 	) {
 	}
 
-	public static function from(BookingReference $reference, Transaction $transaction): self
+	public static function from(BookingReference $reference, ?Transaction $transaction, BookingEmailResult $emailResult): self
 	{
-		return new self($reference, $transaction);
+		return new self($reference, $transaction, $emailResult);
 	}
 }
