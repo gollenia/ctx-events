f093b7c5786b5c216baf779b218a15aa407f73bc
Thomas Gollenia <thomas@gollenia.at>
2026-03-24 14:49:47 +0100

    ModalAction wired correctly

--- src/shared/datatable/Actions.tsx
+++ src/shared/datatable/Actions.tsx
@@ -1,3 +1,4 @@
+import { useState } from '@wordpress/element';
 import clsx from 'clsx';
 import { useDataTable } from './DataTableContext';
 import type { DataTableAction } from './types';
@@ -10,6 +11,9 @@ interface ActionProps {
 
 const Actions = ({ actions, showOnHover, item }: ActionProps) => {
 	const { onChangeView, view } = useDataTable();
+	const [activeModalAction, setActiveModalAction] = useState<DataTableAction | null>(
+		null,
+	);
 
 	const classes: string = clsx(
 		'row-actions',
@@ -26,42 +30,59 @@ const Actions = ({ actions, showOnHover, item }: ActionProps) => {
 			: !action.disabled;
 	});
 
-	return (
-		<div className={classes}>
-			{filteredActions.map((action: DataTableAction, index: number) => {
-				const actionClasses: string = clsx({
-					trash:
-						action.delete instanceof Function
-							? action.delete(item)
-							: action.delete,
-					last: index === filteredActions.length - 1,
-				});
+	const ActiveModal = activeModalAction?.RenderModal ?? null;
 
-				if (
-					action.disabled instanceof Function
-						? action.disabled(item)
-						: action.disabled
-				) {
-					return null;
-				}
-				return (
-					<span className={actionClasses} key={index}>
-						<a
-							href="#"
-							onClick={(e) => {
-								e.preventDefault();
-								action.callback([item], onActionPerformed);
-							}}
-						>
-							{action.label instanceof Function
-								? action.label(item)
-								: action.label}
-						</a>
-						{index < filteredActions.length - 1 && '\u00A0|\u00A0'}
-					</span>
-				);
-			})}
-		</div>
+	return (
+		<>
+			<div className={classes}>
+				{filteredActions.map((action: DataTableAction, index: number) => {
+					const actionClasses: string = clsx({
+						trash:
+							action.delete instanceof Function
+								? action.delete(item)
+								: action.delete,
+						last: index === filteredActions.length - 1,
+					});
+
+					if (
+						action.disabled instanceof Function
+							? action.disabled(item)
+							: action.disabled
+					) {
+						return null;
+					}
+					return (
+						<span className={actionClasses} key={index}>
+							<a
+								href="#"
+								onClick={(e) => {
+									e.preventDefault();
+									if (action.RenderModal) {
+										setActiveModalAction(action);
+										return;
+									}
+
+									action.callback([item], onActionPerformed);
+								}}
+							>
+								{action.label instanceof Function
+									? action.label(item)
+									: action.label}
+							</a>
+							{index < filteredActions.length - 1 && '\u00A0|\u00A0'}
+						</span>
+					);
+				})}
+			</div>
+			{ActiveModal && item ? (
+				<ActiveModal
+					action={activeModalAction}
+					item={item}
+					onActionPerformed={onActionPerformed}
+					onClose={() => setActiveModalAction(null)}
+				/>
+			) : null}
+		</>
 	);
 };
 
