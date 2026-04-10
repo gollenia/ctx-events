43e8c9bd31b460a85b7b20e32cbe199a92c73720
Thomas Gollenia <thomas@gollenia.at>
2026-04-10 07:42:51 +0200

    Rename to ctx-events

--- ctx-events.php
+++ ctx-events.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 /*
 Plugin Name: Events
-Plugin URI: https://github.com/gollenia/events
+Plugin URI: https://github.com/gollenia/ctx-events
 Description: Modern event and booking management for WordPress. Easily create events, manage attendees, track availability and handle payments
 Version: 1.0.0
 Requires at least: 6.8.0
@@ -12,7 +12,7 @@ Requires PHP: 8.5
 License: GPL3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 Author: Thomas Gollenia
-Author URI: https://github.com/gollenia/events
+Author URI: https://github.com/gollenia/
 Text Domain: ctx-events
 Domain Path: /languages
 */
