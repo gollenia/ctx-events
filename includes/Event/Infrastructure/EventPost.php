785d1d3c8dd1bcb2c6a6edfd956e8aa4ea0a2fdf
Thomas Gollenia <thomas@gollenia.at>
2026-04-23 07:32:54 +0200

    show in admin bar

--- includes/Event/Infrastructure/EventPost.php
+++ includes/Event/Infrastructure/EventPost.php
@@ -114,6 +114,7 @@ class EventPost extends PostType implements HasTaxonomies, HasMetaData, HasHooks
             'show_in_menu' => false,
             'show_in_rest' => true,
             'show_in_nav_menus' => true,
+			'show_in_admin_bar' => true,
             'can_export' => true,
             'exclude_from_search' => false,
             'publicly_queryable' => true,
