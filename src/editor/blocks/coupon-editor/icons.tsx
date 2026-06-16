8376f79f5e0fa0843116dc94e8db3e518e83a3c3
Thomas Gollenia <thomas@gollenia.at>
2026-06-16 08:47:46 +0200

    Refactor icon export in coupon editor block

--- src/editor/blocks/coupon-editor/icons.tsx
+++ src/editor/blocks/coupon-editor/icons.tsx
@@ -1,7 +1,8 @@
+import type { BlockTypeIconDescriptor } from '@wordpress/blocks/build-types';
 import { Path, SVG } from '@wordpress/components';
 
-const icons = {
-	posts: (
+const posts: BlockTypeIconDescriptor = {
+	src: (
 		<SVG
 			xmlns="http://www.w3.org/2000/svg"
 			enableBackground="new 0 0 24 24"
@@ -27,4 +28,4 @@ const icons = {
 	),
 };
 
-export default icons;
+export { posts };
