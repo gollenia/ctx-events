ed53d9d10285d7ab8ceac8a68e723492a02dc242
Thomas Gollenia <thomas@gollenia.at>
2026-09-03 10:36:37 +0200

    removed ts generate from build

--- ctx-events.php
+++ ctx-events.php
@@ -6,8 +6,9 @@ declare(strict_types=1);
 Plugin Name: Events
 Plugin URI: https://github.com/gollenia/ctx-events
 Description: Modern event and booking management for WordPress. Easily create events, manage attendees, track availability and handle payments
-Version: 0.1.3
-Requires at least: 6.9.0
+Version: 0.1.4
+Requires at least: 7.1.0
+Tested up to: 7.1.0
 Requires PHP: 8.5
 License: GPL3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
